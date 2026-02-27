1165956f5f0d01cb4d69229d23ded0ca3e2125e6
Raushanmehta <raushanmehta2184@gmail.com>
2026-02-28 03:50:46 +0530

    revidion

--- src/Revision.java
+++ src/Revision.java
@@ -5,12 +5,15 @@ import java.util.List;
 public class Revision {
     public static void main(String[] args) {
 
-    int num = 123456, rev = 0;
-    while (num!= 0){
-        rev = rev*10  + num%10;
-         num/=10;
-    }
-    System.out.println(rev);
+      int [] arr = {5,9,3,1};
+      int sum = 0;
+
+      for (int i = 0; i < arr.length; i++) {
+          sum = sum + arr[i];
+
+      }
+        System.out.println(sum);
+
 
     }
 }
